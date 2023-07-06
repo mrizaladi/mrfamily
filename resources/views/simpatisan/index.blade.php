a21bd80f4c26e215223639aca02267d6635b2859
Rizal Adi <rizaladi.nu@gmail.com>
2023-07-06 11:22:04 +0700

    modified icon for simpatisan page

--- resources/views/simpatisan/index.blade.php
+++ resources/views/simpatisan/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
 <div class="container-xl">
     <div class="page-header d-print-none">
         <h2 class="page-title">
@@ -11,28 +10,24 @@
 </div>
 <div class="page-body">
     <div class="container-xl">
-
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">Data Simpatisan</h3>
                 <div class="card-actions">
-                    <a href="" class="btn btn-azure d-none d-sm-inline-block">
+                    <a href="#" class="btn btn-azure btn-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-upload" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"></path>
                             <path d="M7 9l5 -5l5 5"></path>
                             <path d="M12 4l0 12"></path>
                         </svg>
-                        Import
                     </a>
-                    <a href="{{ route('simpatisan.create') }}" class="btn btn-primary d-none d-sm-inline-block">
-                        <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                    <a href="{{ route('simpatisan.create') }}" class="btn btn-primary btn-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M12 5l0 14"></path>
                             <path d="M5 12l14 0"></path>
                         </svg>
-                        Tambah
                     </a>
                 </div>
             </div>
