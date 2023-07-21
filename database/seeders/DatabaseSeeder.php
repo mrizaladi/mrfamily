7d2ac3fe38809250c9f7c7406cfde29cfd7dab2d
Rizal Adi <rizaladi.nu@gmail.com>
2023-07-22 00:03:34 +0700

    add laravel-spatie permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        $superadminRole = Role::create(['name' => 'superadmin']);
+        $adminRole = Role::create(['name' => 'admin']);
+
+
+        $admin = User::create([
             'name' => 'admin',
             'email' => 'admin@mrfamily.com',
             'password' => Hash::make('admin'),
@@ -21,6 +29,27 @@ class DatabaseSeeder extends Seeder
             'district_id' => '1',
             'subdistrict_id' => '1'
         ]);
+
+        $superadmin = User::create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@mrfamily.com',
+            'password' => Hash::make('superadmin'),
+            'regency_id' => '1',
+            'district_id' => '1',
+            'subdistrict_id' => '1'
+        ]);
+
+        $admin->assignRole('admin');
+        $superadmin->assignRole('superadmin');
+
+        // Create permissions
+        Permission::create(['name' => 'access all pages']);
+        Permission::create(['name' => 'access tps page']);
+        Permission::create(['name' => 'access simpatisan page']);
+
+        // Assign permissions to roles
+        $superadminRole->givePermissionTo('access all pages');
+        $adminRole->givePermissionTo('access tps page', 'access simpatisan page');
 
         $this->call([
             RegencySeeder::class,
