50beee41ee2f162027b206095b274d268be4fd98
Rizal Adi <rizaladi.nu@gmail.com>
2023-07-25 22:49:06 +0700

    test simpatisan seeder to million in server

--- database/seeders/SimpatisanSeeder.php
+++ database/seeders/SimpatisanSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Simpatisan;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use function PHPSTORM_META\map;
 
 class SimpatisanSeeder extends Seeder
 {
@@ -13,6 +14,27 @@ class SimpatisanSeeder extends Seeder
      */
     public function run(): void
     {
-        Simpatisan::factory()->count(50)->create();
+        // Simpatisan::factory()->count(5000)->create();
+
+        $data = [];
+        for ($i=0; $i < 500000; $i++) { 
+            $data[] = [
+                'updated_at' => now(),
+                'created_at' => now(),
+                'nik' => fake()->nik(),
+                'name' => fake()->name(),
+                'phone' => fake()->phoneNumber(),
+                'sex' => fake()->randomElement(['Laki-Laki', 'Perempuan']),
+                'regency_id' => fake()->randomElement(range(1, 4)),
+                'district_id' => fake()->randomElement(range(1, 10)),
+                'subdistrict_id' => fake()->randomElement(range(1, 10)),
+                'ktp' => fake()->creditCardNumber(),
+                'user_id' => fake()->randomElement(range(1, 3))
+            ];
+        }
+
+        foreach(array_chunk($data, 1000) as $chunk){
+            Simpatisan::insert($chunk);
+        }
     }
 }
