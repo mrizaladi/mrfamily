f80327a8a001285e79b042b0715dd5333d180d47
mashudwahyu <mashudwahyu04@gmail.com>
2023-12-25 21:07:50 +0700

    fix show without pip dan kip

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,10 +29,10 @@ class HomeController extends Controller
         $data['totalSimpatisanSalatiga'] = Simpatisan::where('regency_id', 3)->whereNotIn('user_id', [18,19,20,21])->whereNotNull('nik')->count();
         $data['totalSimpatisanKotaSemarang'] = Simpatisan::where('regency_id', 4)->whereNotIn('user_id', [18,19,20,21])->whereNotNull('nik')->count();
 
-        $data['totalPemilihKendal'] = Simpatisan::where('regency_id', 1)->whereNotIn('user_id', [18,19,20,21])->count();
-        $data['totalPemilihKabSemarang'] = Simpatisan::where('regency_id', 2)->whereNotIn('user_id', [18,19,20,21])->count();
-        $data['totalPemilihSalatiga'] = Simpatisan::where('regency_id', 3)->whereNotIn('user_id', [18,19,20,21])->count();
-        $data['totalPemilihKotaSemarang'] = Simpatisan::where('regency_id', 4)->whereNotIn('user_id', [18,19,20,21])->count();
+        $data['totalPemilihKendal'] = Simpatisan::where('regency_id', 1)->count();
+        $data['totalPemilihKabSemarang'] = Simpatisan::where('regency_id', 2)->count();
+        $data['totalPemilihSalatiga'] = Simpatisan::where('regency_id', 3)->count();
+        $data['totalPemilihKotaSemarang'] = Simpatisan::where('regency_id', 4)->count();
         return view('home', $data);
     }
 }
