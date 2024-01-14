c6a9985b6d6fc99566c4ea2fa8b7a9f9badec864
Rizal Adi <rizaladi.nu@gmail.com>
2024-01-14 14:03:13 +0700

    fix: add filter kecamatan, kelurahan

--- resources/views/tps/create.blade.php
+++ resources/views/tps/create.blade.php
@@ -11,72 +11,75 @@
 </div>
 <div class="page-body">
     <div class="container-xl">
-        <form action="{{ route('tps.store') }}" method="POST" id="tpsForm">
+        <form action="{{ route('tps.store') }}" method="POST" id="tpsForm" enctype="multipart/form-data">
             @csrf
             <div class="card">
                 <div class="card-body">
                     <div class="row row-cards">
                         <div class="col-sm-6 col-md-3">
                             <div class="mb-3">
-                                <label class="form-label required">Kabupaten/Kota</label>
-                                <select class="form-control form-select" name="regency_id">
-                                    <option value="">Pilih Kabupaten / Kota</option>
-                                    @foreach ($regencies as $regency)
-                                    <option value="{{ $regency->id }}">{{ $regency->name }}</option>
-                                    @endforeach
-                                </select>
+                                 <label class="form-label required">Kabupaten/Kota</label>
+                                 <select class="form-control form-select @error('regency_id') is-invalid @enderror" name="regency_id" id="regency">
+                                     <option value="">Pilih Kabupaten / Kota</option>
+                                     @foreach ($regencies as $regency)
+                                     <option value="{{ $regency->id }}" {{ old('regency_id') == $regency->id ? 'selected' : '' }}>{{ $regency->name }}</option>
+                                     @endforeach
+                                 </select>
+                                 @error('regency_id')
+                                 <div class="invalid-feedback">{{ $message }}</div>
+                                 @enderror
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-3">
                             <div class="mb-3">
-                                <label class="form-label required">Kecamatan</label>
-                                <select class="form-control form-select" name="district_id">
-                                    <option value="">Pilih Kecamatan</option>
-                                    @foreach ($districts as $district)
-                                    <option value="{{ $district->id }}">{{ $district->name }}</option>
-                                    @endforeach
-                                </select>
+                                 <label class="form-label required">Kecamatan</label>
+                                 <select class="form-control form-select @error('district_id') is-invalid @enderror" name="district_id" id="district">
+                                     <option value="">Pilih Kabupaten</option>
+                                 </select>
+                                 @error('district_id')
+                                 <div class="invalid-feedback">{{ $message }}</div>
+                                 @enderror
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-3">
                             <div class="mb-3">
-                                <label class="form-label required">Kelurahan</label>
-                                <select class="form-control form-select" name="subdistrict_id">
-                                    <option value="">Pilih Kelurahan</option>
-                                    @foreach ($subdistricts as $subdistrict)
-                                    <option value="{{ $subdistrict->id }}">{{ $subdistrict->name }}</option>
-                                    @endforeach
-                                </select>
+                                 <label class="form-label required">Desa/Kelurahan</label>
+                                 <select class="form-control form-select @error('subdistrict_id') is-invalid @enderror" name="subdistrict_id" id="subdistrict">
+                                     <option value="">Pilih Desa/Kelurahan</option>
+                                 </select>
+                                 @error('subdistrict_id')
+                                 <div class="invalid-feedback">{{ $message }}</div>
+                                 @enderror
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-3">
                             <div class="mb-3">
                                 <label class="form-label required">Desa</label>
-                                <input type="text" class="form-control" placeholder="Desa" name="village">
+                                <input type="text" class="form-control" placeholder="Desa" name="village" required>
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-3">
                             <div class="mb-3">
                                 <label class="form-label required">Nama Petugas</label>
-                                <input type="text" class="form-control" placeholder="Nama Petugas" name="officer">
+                                <input type="text" class="form-control" placeholder="Nama Petugas" name="officer" required>
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-2">
                             <div class="mb-3">
                                 <label class="form-label required">No TPS</label>
-                                <input type="number" class="form-control" placeholder="No TPS" name="tps">
+                                <input type="number" class="form-control" placeholder="No TPS" name="tps" required>
                             </div>
                         </div>
                         <div class="col-sm-6 col-md-2">
                             <div class="mb-3">
                                 <label class="form-label required">Jumlah Pemilih</label>
-                                <input type="number" class="form-control" placeholder="Jumlah Pemilih" name="total_voters">
+                                <input type="number" class="form-control" placeholder="Jumlah Pemilih" name="total_voters" required>
                             </div>
                         </div>
 
                         <div class="col-sm-4 col-md-2">
                             <div class="mb-3">
-                                <label class="form-label required">Foto Petugas</label>
+                                <label class="form-label">Foto Petugas</label>
                                 <input type="file" class="form-control">
                             </div>
                         </div>
@@ -107,6 +110,62 @@
     function resetForm() {
         document.getElementById("tpsForm").reset();
     }
+
+    $(document).ready(function() {
+        $('#regency').on('change', function() {
+            var idRegency = this.value;
+            console.log(idRegency);
+            $("#district").html('');
+            $.ajax({
+                url: '/district/' + idRegency
+                , type: "GET"
+                , data: {}
+                , dataType: 'json'
+                , success: function(result) {
+                    if (result) {
+                        $('#district').empty();
+                        $('#district').append('<option hidden>Pilih Kecamatan</option>');
+                        $.each(result, function(key, value) {
+                            $('select[name="district_id"]').append('<option value="' + value.id + '">' + value.name + '</option>');
+                        });
+                        $('#district').val("{{ old('district_id') }}").trigger('change');
+                    } else {
+                        $('#district').empty();
+                    }
+                }
+            });
+        });
+        $('#district').on('change', function() {
+            var idRegency = this.value;
+            console.log(idRegency);
+            $("#subdistrict").html('');
+            $.ajax({
+                url: '/subdistrict/' + idRegency
+                , type: "GET"
+                , data: {}
+                , dataType: 'json'
+                , success: function(res) {
+                    if (res) {
+                        $('#subdistrict').empty();
+                        $('#subdistrict').append('<option hidden>Pilih Kecamatan</option>');
+                        $.each(res, function(key, val) {
+                            $('select[name="subdistrict_id"]').append('<option value="' + val.id + '">' + val.name + '</option>');
+                        });
+                        $('#subdistrict').val("{{ old('subdistrict_id') }}").trigger('change');
+                    } else {
+                        $('#subdistrict').empty();
+                    }
+                }
+            });
+        });
+        if ("{{ old('regency_id')}}") {
+            console.log("{{ old('regency_id')}}");
+            $('#regency').trigger('change');
+        }
+    });
+
 </script>
 @endsection
+
+
 
